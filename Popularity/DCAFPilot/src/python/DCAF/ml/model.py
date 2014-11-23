32270fa45a8302b18bf2d2cb0f178b5239a82cd1
Valentin Kuznetsov <vkuznet@gmail.com>
2014-11-23 15:40:40 -0500

    Change classifiers to learners, add scaler/scorers

--- Popularity/DCAFPilot/src/python/DCAF/ml/model.py
+++ Popularity/DCAFPilot/src/python/DCAF/ml/model.py
@@ -23,11 +23,14 @@ import pandas as pd
 
 # sklearn modules
 from sklearn.cross_validation import train_test_split
+from sklearn import preprocessing
+from sklearn.metrics.scorer import SCORERS
+from sklearn.pipeline import make_pipeline
 from sklearn import metrics
 
 # local modules
 from DCAF.ml.utils import OptionParser, normalize, logloss, GLF
-from DCAF.ml.clf import classifiers, param_search, crossvalidation, print_clf_report
+from DCAF.ml.clf import learners, param_search, crossvalidation, print_clf_report
 import DCAF.utils.jsonwrapper as json
 
 def get_auc(labels, predictions):
@@ -35,7 +38,7 @@ def get_auc(labels, predictions):
     auc = metrics.auc(fpr,tpr)
     return auc
 
-def read_data(fname, drops=[]):
+def read_data(fname, drops=[], idx=0, limit=-1):
     "Read and return processed data frame"
     xdf = pd.read_csv(fname)
     # fill NAs
@@ -45,6 +48,8 @@ def read_data(fname, drops=[]):
         xdf = xdf.drop(drops, axis=1)
     # drop duplicates
     xdf = xdf.drop_duplicates(take_last=True, inplace=False)
+    if  limit > -1:
+        xdf = xdf[idx:limit]
     return xdf
 
 def factorize(col, xdf, sdf=None):
@@ -62,28 +67,30 @@ def factorize(col, xdf, sdf=None):
         out.append(dval[val])
     return out
 
-def model(train_file, test_file, clf_name, clf_params=None,
-        idx=0, limit=-1, gsearch=None, crossval=None, verbose=False):
+def model(train_file, test_file, learner, lparams=None, scorer=None,
+        scaler=None, idx=0, limit=-1, gsearch=None, crossval=None, verbose=False):
     """
     Build and run ML algorihtm for given train/test dataframe
-    and classifier name. The classifiers are defined externally
+    and classifier name. The learners are defined externally
     in DCAF.ml.clf module.
     """
-    clf = classifiers()[clf_name]
-    if  clf_params:
-        if  isinstance(clf_params, str):
-            clf_params = json.loads(clf_params)
-        elif isinstance(clf_params, dict):
+    if  scaler:
+        scaler = getattr(preprocessing, scaler)
+    clf = learners()[learner]
+    if  lparams:
+        if  isinstance(lparams, str):
+            lparams = json.loads(lparams)
+        elif isinstance(lparams, dict):
             pass
         else:
-            raise Exception('Invalid data type for clf_params="%s", type: %s' % (clf_params, type(clf_params)))
-        for key, val in clf_params.items():
+            raise Exception('Invalid data type for lparams="%s", type: %s' % (lparams, type(lparams)))
+        for key, val in lparams.items():
             setattr(clf, key, val)
     print "clf:", clf
 
     # read data and normalize it
     drops = []
-    xdf = read_data(train_file, drops)
+    xdf = read_data(train_file, drops, idx, limit)
     if  test_file:
         tdf = read_data(test_file, drops)
     else:
@@ -92,16 +99,6 @@ def model(train_file, test_file, clf_name, clf_params=None,
         xdf[col] = factorize(col, xdf, tdf)
         if  tdf:
             tdf[col] = factorize(col, tdf, xdf)
-
-    # normalize some columns
-    columns = []
-#    if  train_file and test_file:
-#        normalize(columns, xdf, tdf)
-#    print "train file:"
-#    print xdf.ix[1]
-#    if  test_file:
-#        print "test file:"
-#        print tdf.ix[1]
 
     # get target variable and exclude choice from train data
     tcol = 'target' # classification column name (what we'll predict)
@@ -114,11 +111,6 @@ def model(train_file, test_file, clf_name, clf_params=None,
     # split our train data
     x_train, x_rest, y_train, y_rest = \
             train_test_split(xdf, target, test_size=0.33)
-    if  limit > -1 and x_rest.any():
-        x_train = x_train[idx:limit]
-        y_train = y_train[idx:limit]
-        x_rest = x_rest[idx:limit]
-        y_rest = y_rest[idx:limit]
     if  verbose:
         print "train shapes:", x_train.shape, y_train.shape
     if  gsearch:
@@ -128,10 +120,13 @@ def model(train_file, test_file, clf_name, clf_params=None,
         crossvalidation(clf, xdf, target)
         sys.exit(0)
 
-    if  clf_name == 'rfc10':
-        bfeatures = best_features(xdf, clf)
+    if  scaler:
+        x_train = scaler.fit_transform(x_train)
     fit = clf.fit(x_train, y_train)
     predictions = fit.predict(x_rest)
+    if  scorer:
+        print "Score metric: %s" % scorer
+        print metrics.SCORERS[scorer](clf, x_rest, y_rest)
     loss = 0
     tot = 0
     for pval, yval in zip(predictions, y_rest):
@@ -151,14 +146,81 @@ def model(train_file, test_file, clf_name, clf_params=None,
         ofile = 'prediction.txt'
         out.to_csv(ofile, header=False, index=False)
 
+def model_iter(train_file_list, test_file, learner, lparams=None, scaler=None, verbose=False):
+    """
+    Build and run ML algorihtm for given train/test dataframe
+    and classifier name. The learners are defined externally
+    in DCAF.ml.clf module.
+    """
+    if  learner not in ['SGDClassifier', 'SGDRegressor']:
+        raise Exception("Unsupported learner %s" % learner)
+    if  scaler:
+        scaler = getattr(preprocessing, scaler)
+    clf = learners()[learner]
+    if  lparams:
+        if  isinstance(lparams, str):
+            lparams = json.loads(lparams)
+        elif isinstance(lparams, dict):
+            pass
+        else:
+            raise Exception('Invalid data type for lparams="%s", type: %s' % (lparams, type(lparams)))
+        for key, val in lparams.items():
+            setattr(clf, key, val)
+    print "clf:", clf
+
+    drops = []
+    tcol = 'target' # classification column name (what we'll predict)
+    fit = None
+    for train_file in train_file_list:
+        # read data and normalize it
+        xdf = read_data(train_file, drops)
+        if  test_file:
+            tdf = read_data(test_file, drops)
+        else:
+            tdf = None
+        for col in ['era', 'primds', 'procds']:
+            xdf[col] = factorize(col, xdf, tdf)
+            if  tdf:
+                tdf[col] = factorize(col, tdf, xdf)
+
+        # get target variable and exclude choice from train data
+        target = xdf[tcol]
+        xdf = xdf.drop(tcol, axis=1)
+        if  verbose:
+            print "Columns:", ','.join(xdf.columns)
+            print "Target:", target
+
+        x_train = xdf
+        if  scaler:
+            x_train = scaler.fit_transform(xdf)
+        y_train = target
+        fit = clf.partial_fit(x_train, y_train)
+
+    # test data
+    if  test_file:
+        predictions = fit.predict(tdf)
+        if  not isinstance(predictions[0], int):
+            predictions = [int(round(i)) for i in predictions]
+        out = pd.DataFrame(zip(tdf['dataset'], predictions))
+        ofile = 'prediction.txt'
+        out.to_csv(ofile, header=False, index=False)
+
 def main():
     "Main function"
-    optmgr = OptionParser(classifiers().keys())
+    optmgr = OptionParser(learners().keys(), SCORERS.keys())
     opts, _ = optmgr.options()
-    model(train_file=opts.train, test_file=opts.test,
-            clf_name=opts.clf, clf_params=opts.clf_params,
-            idx=opts.idx, limit=opts.limit, gsearch=opts.gsearch,
-            crossval=opts.cv, verbose=opts.verbose)
+    if  opts.train.find(',') != -1: # list of files
+        train_files = opts.train.split(',')
+        print "Train file list", train_files
+        model_iter(train_file_list=train_files, test_file=opts.test,
+                learner=opts.learner, lparams=opts.lparams,
+                scaler=opts.scaler, verbose=opts.verbose)
+    else:
+        model(train_file=opts.train, test_file=opts.test,
+                learner=opts.learner, lparams=opts.lparams,
+                scorer=opts.scorer, scaler=opts.scaler,
+                idx=opts.idx, limit=opts.limit, gsearch=opts.gsearch,
+                crossval=opts.cv, verbose=opts.verbose)
 
 if __name__ == '__main__':
     main()
