71e246e59759d139c7953d9ee22e3a03946f708a
Valentin Kuznetsov <vkuznet@gmail.com>
2014-11-25 13:19:15 -0500

    Add new tool to generate dates

---
+++ Popularity/DCAFPilot/src/python/DCAF/tools/dates.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+#pylint: disable=
+"""
+File       : dates.py
+Author     : Valentin Kuznetsov <vkuznet AT gmail dot com>
+Description: This module generates series of days in specific
+             interval starting from given date.
+"""
+
+# system modules
+import os
+import sys
+import time
+import datetime
+import optparse
+import calendar
+
+# package modules
+from DCAF.utils.utils import dates
+
+class OptionParser():
+    def __init__(self):
+        "User based option parser"
+        self.parser = optparse.OptionParser()
+        self.parser.add_option("--start", action="store", type="string",
+            dest="start", default="", help="Input start date (YYYYMMDD)")
+        self.parser.add_option("--ndays", action="store", type="int",
+            dest="ndays", default=7, help="Dates interval, default 7 days")
+    def get_opt(self):
+        "Return list of options"
+        return self.parser.parse_args()
+
+def main():
+    "Main function"
+    optmgr  = OptionParser()
+    opts, _ = optmgr.get_opt()
+    start_date = opts.start
+    ndays = opts.ndays
+    if  not start_date:
+        print "Please specify start date, see --help for more options"
+        sys.exit(1)
+    for pair in dates(start_date, ndays):
+        print '%s %s' % (pair[0], pair[1])
+
+if __name__ == '__main__':
+    main()
