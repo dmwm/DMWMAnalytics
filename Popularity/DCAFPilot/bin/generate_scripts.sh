f04f56d34b525865e08d98b4c9e1d20b9cd2f464
Valentin Kuznetsov <vkuznet@gmail.com>
2014-11-25 13:19:52 -0500

    New script to generate run-time bash scripts

---
+++ Popularity/DCAFPilot/bin/generate_scripts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+start=$1
+if [ -z $start ]; then
+    echo "Please specify input date"
+    exit
+fi
+echo "#!/bin/bash"
+src/python/DCAF/tools/dates.py --start=$start \
+    | awk '{print "nohup time ./bin/dataframe --start="$1" --stop="$2" --dbs-extra=100 --verbose=1 --fout=dataframe-"$1"-"$2".csv 2>&1 1>& dataframe-"$1"-"$2".log < /dev/null &"}'
+
