c0b17276dfc4574a64aca13741d3957e3f74ac97
Valentin Kuznetsov <vkuznet@gmail.com>
2014-11-26 09:29:58 -0500

    Rename update to seed-cache

--- Popularity/DCAFPilot/bin/generate_scripts.sh
+++ Popularity/DCAFPilot/bin/generate_scripts.sh
@@ -6,7 +6,7 @@ if [ -z $start ]; then
     exit
 fi
 echo "#!/bin/bash"
-echo "dataframe --update"
+echo "dataframe --seed-cache"
 src/python/DCAF/tools/dates.py --start=$start \
     | awk '{print "nohup time dataframe --start="$1" --stop="$2" --dbs-extra="DBSEXTRA" --verbose=1 --fout=dataframe-"$1"-"$2".csv 2>&1 1>& dataframe-"$1"-"$2".log < /dev/null &"}' DBSEXTRA=$dbsextra
 
