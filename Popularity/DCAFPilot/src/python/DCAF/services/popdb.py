30443fddccbf7031ce272df42c081a5ad4b2ba16
Valentin Kuznetsov <vkuznet@gmail.com>
2014-11-30 18:16:43 -0500

    Pass parameters into url

--- Popularity/DCAFPilot/src/python/DCAF/services/popdb.py
+++ Popularity/DCAFPilot/src/python/DCAF/services/popdb.py
@@ -29,7 +29,7 @@ class PopDBService(GenericService):
 
     def fetch(self, api, params=None):
         "Fetch data for given api"
-        url = '%s/%s' % (self.url, api)
+        url = '%s/%s?%s' % (self.url, api, urllib.urlencode(params, doseq=True))
         docid = genkey("url=%s params=%s" % (url, params))
         res = self.storage.fetch_one('cache', {'_id':docid})
         if  res and 'data' in res:
