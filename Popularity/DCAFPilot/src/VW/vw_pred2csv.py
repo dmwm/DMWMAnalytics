20f88d2e790cc6b9ec70ff30a87ad06dc611468e
Valentin Kuznetsov <vkuznet@gmail.com>
2014-12-03 15:00:49 -0500

    Extend script to yeild dataset,dbs,prediction

--- Popularity/DCAFPilot/src/VW/vw_pred2csv.py
+++ Popularity/DCAFPilot/src/VW/vw_pred2csv.py
@@ -17,28 +17,42 @@ class OptionParser():
         "User based option parser"
         self.parser = optparse.OptionParser()
         self.parser.add_option("--fin", action="store", type="string",
-            dest="fin", default="", help="Input file")
+            dest="fin", default="", help="Input prediction file")
+        self.parser.add_option("--fvw", action="store", type="string",
+            dest="fvw", default="", help="Input VW file")
         self.parser.add_option("--fout", action="store", type="string",
             dest="fout", default="", help="Output file")
         self.parser.add_option("--headers", action="store", type="string",
-            dest="headers", default="id,prediction", help="Headers for CSV file (comma separated), default id,prediction")
+            dest="headers", default="dataset,dbs,prediction", help="Headers for CSV file (comma separated), default dataset,dbs,prediction")
     def get_opt(self):
         "Return list of options"
         return self.parser.parse_args()
 
-def convert(fin, fout, headers):
+def get_dbs(vwrow):
+    "Extract dbs id from given VW row"
+    for item in vwrow.split():
+        if  item.startswith('dbs'):
+            return item.split(':')[-1]
+    return 0
+def convert(fin, fvw, fout, headers):
     "Function which convert VW input file into CSV output one"
-    with open(fin, 'r') as istream, open(fout, 'w') as ostream:
+    with open(fin, 'r') as istream, open(fvw, 'r') as wstream, open(fout, 'w') as ostream:
         ostream.write(headers+'\n')
-        for line in istream.readlines():
-            row = line.replace('\n', '').split()
-            ostream.write("%s,%s\n" % (row[-1], row[0]))
+        while True:
+            try:
+                vwrow = wstream.readline()
+                if  not vwrow:
+                    break
+                row = istream.readline().replace('\n', '').split()
+                ostream.write("%s,%s,%s\n" % (row[-1], get_dbs(vwrow), row[0]))
+            except:
+                break
 
 def main():
     "Main function"
     optmgr  = OptionParser()
     opts, _ = optmgr.get_opt()
-    convert(opts.fin, opts.fout, opts.headers)
+    convert(opts.fin, opts.fvw, opts.fout, opts.headers)
 
 if __name__ == '__main__':
     main()
