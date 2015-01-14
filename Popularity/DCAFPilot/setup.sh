28ed99b00bc53fe1cc7c77864e6f1bac9ed95f8c
Valentin <vkuznet@gmail.com>
2015-01-14 01:02:55 +0100

    Add setup for das init (it set up mongodb)

--- Popularity/DCAFPilot/setup.sh
+++ Popularity/DCAFPilot/setup.sh
@@ -2,3 +2,6 @@
 export PATH=$PATH:$PWD
 export PYTHONPATH=$PWD/src/python:$PYTHONPATH
 export PATH=$PWD/bin:$PATH
+if [ -f /data/srv/current/apps/das/etc/profile.d/init.sh ]; then
+source /data/srv/current/apps/das/etc/profile.d/init.sh
+fi
