0f718486b2716160ca5195c52736a2672eb98259
Valentin Kuznetsov <vkuznet@gmail.com>
2015-01-30 11:52:52 -0500

    Adjust to new new_datasets CSV file format (dataset,dbs)

--- Popularity/DCAFPilot/src/python/DCAF/tools/slice_data.py
+++ Popularity/DCAFPilot/src/python/DCAF/tools/slice_data.py
@@ -24,41 +24,48 @@ from DCAF.utils.utils import fopen
 class OptionParser():
     def __init__(self):
         "User based option parser"
-        self.parser = optparse.OptionParser()
+        usage  = "Usage: %prog [options]\n"
+        usage += "Description: slice train/newdata files with respect to prodived dataset/dbs set"
+        self.parser = optparse.OptionParser(usage=usage)
         self.parser.add_option("--pred", action="store", type="string",
             dest="pred", default="", help="prediction file")
         self.parser.add_option("--data", action="store", type="string",
             dest="data", default="", help="dataset file")
         self.parser.add_option("--ids", action="store", type="string",
-            dest="ids", default="", help="ids file")
+            dest="ids", default="", help="ids file which contains unique dataset/dbs pairs (it can be produced via new_datasets script)")
     def get_opt(self):
         "Return list of options"
         return self.parser.parse_args()
 
 def select(pred, data, ids):
     "Select prediction and data based on provided ids"
-    sids = set([str(i.replace('\n','')) for i in myopen(ids).readlines() if not i.startswith('id')])
+    sids = [i.replace('\n','').split(',') for i in fopen(ids).readlines() if not i.startswith('dataset')]
     ipred = fopen(pred, 'r')
     idata = fopen(data, 'r')
     pheaders = []
     dheaders = []
     didx = 0
-    with fopen('new_pred.txt', 'wb') as opred, fopen('new_data.txt', 'wb') as odata:
-        while True:
-            pline = ipred.readline().replace('\n', '').split(',')
-            dline = idata.readline().replace('\n', '').split(',')
-            if  not dheaders:
-                pheaders = pline
-                dheaders = dline
-                didx = dheaders.index('dataset')
-                opred.write(','.join(pheaders)+'\n')
-                odata.write(','.join(dheaders)+'\n')
-                continue
-            if  len(pline) == 1:
-                break
-            if  pline[0] in sids and dline[didx] in sids and dline[-1]=='1':
-                opred.write(','.join(pline)+'\n')
-                odata.write(','.join(dline)+'\n')
+    with fopen('new_pred.txt', 'wb') as opred:
+        with fopen('new_data.txt', 'wb') as odata:
+            while True:
+                pline = ipred.readline().replace('\n', '').split(',')
+                dline = idata.readline().replace('\n', '').split(',')
+                if  not dheaders:
+                    pheaders = pline
+                    dheaders = dline
+                    didx = dheaders.index('dataset')
+                    ddbs = dheaders.index('dbs')
+                    dpre = dheaders.index('target')
+                    opred.write(','.join(pheaders)+'\n')
+                    odata.write(','.join(dheaders)+'\n')
+                    continue
+                if  len(pline) == 1:
+                    break
+                dataset, dbs, prediction = pline
+                if  [dataset, dbs] in sids and \
+                    [dline[didx], dline[ddbs]] in sids and dline[dpre]=='1':
+                    opred.write(','.join(pline)+'\n')
+                    odata.write(','.join(dline)+'\n')
 
 def main():
     "Main function"
