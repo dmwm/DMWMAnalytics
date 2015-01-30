88762e646d5616582401bb5749831fbe909b85b3
Valentin Kuznetsov <vkuznet@gmail.com>
2015-01-30 11:53:52 -0500

    Add options, but this script can be replaced with new_datasets.py one

--- Popularity/DCAFPilot/src/R/new_datasets.R
+++ Popularity/DCAFPilot/src/R/new_datasets.R
@@ -2,6 +2,20 @@
 # clean-up session parameters
 rm(list=ls())
 
+# default values
+train.file <- FALSE
+valid.file <- FALSE
+
+# read and parse input args
+cmd_args <- commandArgs()
+for (arg in cmd_args) {
+    arg <- as.character(arg)
+    match <- grep("^--train", arg)
+    if (length(match) == 1) train.file <- gsub("--train=", "", arg)
+    match <- grep("^--valid", arg)
+    if (length(match) == 1) valid.file <- gsub("--valid=", "", arg)
+}
+
 # load data
 my.path <- paste0(getwd(), "/")
 # example
@@ -18,8 +32,8 @@ for(i in 1:length(libs)) {
     suppressMessages(library(pkg, character.only = TRUE))
 }
 
-df=filter(fread('train_clf_cond.csv'), target>0)
-ndf=filter(fread('valid_clf_cond.csv'), target>0)
+df=filter(fread(train.file), target>0)
+ndf=filter(fread(valid.file), target>0)
 old.datasets=unlist(lapply(ndf$dataset, function(d) {if((d %in% df$dataset)==TRUE) return(d)}))
 new.datasets=unlist(lapply(ndf$dataset, function(d) {if((d %in% df$dataset)==FALSE) return(d)}))
 write.csv(data.frame(id=new.datasets), gzfile("new_datasets.csv.gz"),row.names=FALSE,quote=FALSE)
