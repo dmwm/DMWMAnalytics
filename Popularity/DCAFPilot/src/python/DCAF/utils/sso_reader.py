676af1fbeb32fd4e4d862cc464686f058184d64c
Valentin Kuznetsov <vkuznet@gmail.com>
2015-02-13 16:44:28 -0500

    Re-factor sso_reader to support cern-get-sso-cookie tool which allows to use proxy file

--- Popularity/DCAFPilot/src/python/DCAF/utils/sso_reader.py
+++ Popularity/DCAFPilot/src/python/DCAF/utils/sso_reader.py
@@ -13,6 +13,7 @@ import cookielib
 import HTMLParser
 import logging
 
+from tempfile import NamedTemporaryFile
 from os.path import expanduser, dirname, realpath
 
 VER=sys.version_info
@@ -23,18 +24,18 @@ if  VER[0] == 2 and VER[1] == 7 and VER[2] >= 9:
     import ssl
     ssl._create_default_https_context = ssl._create_unverified_context
 
-class HTTPSClientAuthHandler(urllib2.HTTPSHandler):  
+class HTTPSClientAuthHandler(urllib2.HTTPSHandler):
     def __init__(self, ckey, cert):
         "HTTPS client authentication handler to Establish HTTPS connection"
-        urllib2.HTTPSHandler.__init__(self)  
+        urllib2.HTTPSHandler.__init__(self)
         self.key = ckey
         self.cert = cert
 
-    def https_open(self, req):  
+    def https_open(self, req):
         "Establish HTTPS connection"
-        return self.do_open(self.getConnection, req)  
+        return self.do_open(self.getConnection, req)
 
-    def getConnection(self, host, timeout=300):  
+    def getConnection(self, host, timeout=300):
         "Establish HTTPS connection"
         return httplib.HTTPSConnection(host, key_file=self.key, cert_file=self.cert)
 
@@ -67,7 +68,7 @@ def getSSOCookie(opener, target_url, cookie, debug):
     post_data_local = []
     for match in re.finditer('input type="hidden" name="([^"]*)" value="([^"]*)"', content):
         post_data_local += [(match.group(1), h.unescape(match.group(2)))]
-  
+
     if not post_data_local:
         raise Exception("error: The page doesn't have the form with security attributes, check 'User-agent' header")
     _getResponse(opener, url, urllib.urlencode(post_data_local), debug=debug).read()
@@ -83,20 +84,79 @@ def getContent(target_url, ckey, cert, post_data=None, method="GET", debug=0):
     # if the access gave an exception, try to get a cookie
     try:
         getSSOCookie(opener, target_url, cookie, debug)
-        hasCookie = True 
+        hasCookie = True
         result = _getResponse(opener, target_url, post_data, method, debug).read()
     finally:
         if hasCookie:
             try:
                 _getResponse(opener, "https://login.cern.ch/adfs/ls/?wa=wsignout1.0").read()
             except:
-                print("Error, could not logout correctly from server") 
+                print("Error, could not logout correctly from server")
     return result
 
-def getdata(url, ckey=None, cert=None, method='GET', postdata=None, debug=0):
+def getdata_old(url, ckey=None, cert=None, method='GET', postdata=None, debug=0):
     "Get data from SSO based data-service"
     if  not ckey and not cert:
         from DCAF.utils.utils import get_key_cert
         ckey, cert = get_key_cert()
     content = getContent(url, ckey, cert, postdata, method, debug)
     return content
+
+def check_tool(tool):
+    for idir in os.environ['PATH'].split(':'):
+        fname = os.path.join(idir, tool)
+        if  os.path.exists(fname):
+            return True
+
+def get_cern_sso_cookie(url, debug=0):
+    """
+    Check existance of certn-get-sso-cookie tool on a system, see
+    http://linux.web.cern.ch/linux/docs/cernssocookie.shtml
+    """
+    cern_tool = 'cern-get-sso-cookie'
+    if  check_tool(cern_tool):
+        cookiefile = NamedTemporaryFile(delete=False)
+        cmd = '%s --krb -r -u "%s" -o %s' % (cern_tool, url, cookiefile.name)
+        if  debug:
+            print "Get CERN SSO cookies", cmd
+        os.system(cmd)
+        cookie = cookielib.MozillaCookieJar(cookiefile.name)
+        cookie.load()
+        cookiefile.close()
+        os.unlink(cookiefile.name)
+        return cookie
+
+def getdata_new(url, params, headers=None, ckey=None, cert=None, debug=0):
+    "Fetch data for given url and set of parameters"
+    if  params:
+        url += '?%s' % urllib.urlencode(params, doseq=True)
+    if  debug:
+        print "getdata:url", url
+    req = urllib2.Request(url)
+    if  headers == None:
+        headers = {'Accept': 'application/json'}
+    if  headers:
+        for key, val in headers.items():
+            req.add_header(key, val)
+
+    handler = HTTPSClientAuthHandler(ckey, cert)
+    cookie = get_cern_sso_cookie(url, debug)
+    if  cookie:
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie), handler)
+    else:
+        opener  = urllib2.build_opener(handler)
+    urllib2.install_opener(opener)
+    data = urllib2.urlopen(req)
+    return data.read()
+
+def getdata(url, ckey=None, cert=None, method='GET', postdata=None, debug=0):
+    """
+    Wrapper around old/new implementation of getdata.
+    The original implementation is pure python based, but it does not
+    work with proxy files, while new implementation uses cern-get-sso-cookie
+    tool to get cookie and then pass it into python library for processing.
+    """
+    if  check_tool('cern-get-sso-cookie'):
+        return getdata_new(url, {}, ckey=ckey, cert=cert, debug=debug)
+    else:
+        return getdata_old(url, ckey, cert, method, postdata, debug)
