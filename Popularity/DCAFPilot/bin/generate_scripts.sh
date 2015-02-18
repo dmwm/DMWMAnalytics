d9b252cedef136d6bfa2e747d9c58e41c894b016
Valentin Kuznetsov <vkuznet@gmail.com>
2015-02-18 11:26:58 -0500

    Add root

--- Popularity/DCAFPilot/bin/generate_scripts.sh
+++ Popularity/DCAFPilot/bin/generate_scripts.sh
@@ -9,7 +9,9 @@ overlap=""
 if [ $# -eq 2 ] && [ "$2"=="overlap" ]; then
     overlap="--overlap"
 fi
+# find out where package is installed on a system
+root=`python -c "import DCAF; print '/'.join(DCAF.__file__.split('/')[:-1])"`
 echo "#!/bin/bash"
 echo "dataframe --seed-cache"
-src/python/DCAF/tools/dates.py --start=$start $overlap \
+$root/src/python/DCAF/tools/dates.py --start=$start $overlap \
     | awk '{print "nohup time dataframe --start="$1" --stop="$2" --dbs-extra="DBSEXTRA" --verbose=1 --fout=dataframe-"$1"-"$2".csv 2>&1 1>& dataframe-"$1"-"$2".log < /dev/null &"}' DBSEXTRA=$dbsextra
