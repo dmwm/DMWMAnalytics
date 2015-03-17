ae9571290e383d5dca91efe4b05ba07c1bf69449
Valentin Kuznetsov <vkuznet@gmail.com>
2015-03-17 16:29:28 +0100

    Fix docstrings and add sort option

--- Popularity/DCAFPilot/src/python/DCAF/tools/tier_lookup.py
+++ Popularity/DCAFPilot/src/python/DCAF/tools/tier_lookup.py
@@ -5,7 +5,7 @@
 """
 File       : dataset_lookup.py
 Author     : Valentin Kuznetsov <vkuznet AT gmail dot com>
-Description: Script which look-up dataset info in local cache
+Description: Script which look-up data-tier info in local cache of datasets
 """
 
 # system modules
@@ -30,15 +30,16 @@ class OptionParser():
         self.parser.add_option("--uri", action="store", type="string", \
             dest="uri", default=uri, help="DCAF cache (MongoDB) uri, default %s" %  uri)
         self.parser.add_option("--sep", action="store", type="string", \
-            dest="sep", default=",", help="Output file separator, default comma")
+            dest="sep", default=" ", help="Output file separator, default space")
+        self.parser.add_option("--sort", action="store", type="string", \
+            dest="sort", default="tier", help="Sort by tier or id, default by tier name")
     def get_opt(self):
         "Return list of options"
         return self.parser.parse_args()
 
-def convert(fin, uri, sep=','):
+def convert(fin, uri, sep=',', sortby='tier'):
     """
-    Convert input prediction file (id,prediction) into (dataset,prediction)
-    by using DCAFPilot cache
+    Convert input data file into list of tiers by using DCAFPilot cache
     """
     client = MongoClient(uri)
     mgr = client['analytics']['datasets']
@@ -60,15 +61,22 @@ def convert(fin, uri, sep=','):
             res = mgr.find_one(spec)
             if  res:
                 _, prim, proc, tier = res['dataset'].split('/')
-                tiers[tier] = row[tier_id]
+                tid = int(row[tier_id])
+                if  sortby == 'tier':
+                    tiers[tier] = tid
+                else:
+                    tiers[tid] = tier
     for tier in sorted(tiers.keys()):
-        print tiers[tier], tier
+        if  sortby == 'tier':
+            print '%s%s%s' % (tiers[tier], sep, tier)
+        else:
+            print '%s%s%s' % (tier, sep, tiers[tier])
 
 def main():
     "Main function"
     optmgr = OptionParser()
     opts, _ = optmgr.get_opt()
-    convert(opts.fin, opts.uri, opts.sep)
+    convert(opts.fin, opts.uri, opts.sep, opts.sort)
 
 if __name__ == '__main__':
     main()
