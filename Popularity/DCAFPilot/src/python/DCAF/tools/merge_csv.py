574938d487c9fee7992149ccfdbf190d310eaa01
mbriliauskas <m.briliauskas@gmail.com>
2016-04-13 17:20:31 +0200

    fix for function of file existence check

--- Popularity/DCAFPilot/src/python/DCAF/tools/merge_csv.py
+++ Popularity/DCAFPilot/src/python/DCAF/tools/merge_csv.py
@@ -67,7 +67,7 @@ def merger(fin, fout, verbose=False):
             filelist = [f for f in files(fin, ext)]
             if  len(filelist):
                 break
-    elif os.path.filexists(fin):
+    elif os.path.isfile(fin): # we got file name
         filelist = [fin]
     if  not filelist:
         print("ERROR; unable to create filelist from %s" % fin)
